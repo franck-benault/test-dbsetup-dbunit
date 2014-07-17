373a60220ca992a93ecc66a2d1b365414804696a
franck-benault <franck-benault@orange.fr>
2014-07-17 20:34:04 +0200

    travis-ci init

--- sample1/src/main/java/net/franckbenault/dbtest/sample/CommonOperations.java
+++ sample1/src/main/java/net/franckbenault/dbtest/sample/CommonOperations.java
@@ -5,6 +5,7 @@ import static com.ninja_squad.dbsetup.Operations.*;
 import com.ninja_squad.dbsetup.operation.Operation;
 
 public class CommonOperations {
+	
     public static final Operation DELETE_ALL = 
         deleteAllFrom("PRODUCT", "VENDOR", "COUNTRY", "USER");
 
