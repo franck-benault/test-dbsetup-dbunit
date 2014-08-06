d635e5efe2015b10f90070e7d61c4a4ed5e3e474
franck-benault <franck-benault@orange.fr>
2014-08-06 22:33:37 +0200

    mod05 first init of the project

---
+++ mod05-DBSetupTracker/src/test/java/net/franckbenault/dbtest/sample/DBSetupCommonOperations.java
@@ -0,0 +1,22 @@
+package net.franckbenault.dbtest.sample;
+
+import static com.ninja_squad.dbsetup.Operations.*;
+
+
+import com.ninja_squad.dbsetup.operation.Operation;
+
+public class DBSetupCommonOperations {
+	
+    public static final Operation DELETE_ALL = 
+        deleteAllFrom("USERS");
+    
+    
+    public static final Operation INSERT_USERS_DATA =
+                insertInto("USERS")
+                    .columns("ID","LOGIN", "PASSWORD")
+                    .values(1L,"root", "pwd")
+                    .values(2L,"guest", "pwd")
+                    .values(3L,"guest", "pwd")
+                    .build();
+
+}
