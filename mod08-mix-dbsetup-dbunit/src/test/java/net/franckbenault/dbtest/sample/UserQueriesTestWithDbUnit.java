819a60fc21568278d1b7802fb65d180c1f2bca97
franck-benault <franck-benault@orange.fr>
2014-09-26 21:37:47 +0200

    mod08 remove unless test

--- mod08-mix-dbsetup-dbunit/src/test/java/net/franckbenault/dbtest/sample/UserQueriesTestWithDbUnit.java
+++ mod08-mix-dbsetup-dbunit/src/test/java/net/franckbenault/dbtest/sample/UserQueriesTestWithDbUnit.java
@@ -3,20 +3,15 @@ package net.franckbenault.dbtest.sample;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.List;
 
 import org.dbunit.DatabaseUnitException;
-import org.dbunit.assertion.DbUnitAssert;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
-import org.dbunit.dataset.ITable;
-import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.dbunit.dataset.xml.XmlDataSet;
 import org.dbunit.operation.DatabaseOperation;
 import org.junit.AfterClass;
@@ -71,73 +66,12 @@ public class UserQueriesTestWithDbUnit {
 		assertNotNull(user);
 	}
 	
-	@Test
-	public void testFindUserByLogin2() {
-		User user = userQueries.findUserByLogin("guest");
-		assertNotNull(user);
-	}
-	
+
 	@Test
 	public void testFindUserByLogin_NotFound() {
 		User user = userQueries.findUserByLogin("doesNotExist");
 		assertNull(user);
 	}
 	
-	@Test
-	public void testToString() {
-		User user = new User(1,"MyLogin", "pwd");
-		assertNotNull(user.toString());
-		assertTrue(user.toString().contains("MyLogin"));
-	}
-
-	
-	@Test
-	public void testDeleteUsers() throws DatabaseUnitException, SQLException, FileNotFoundException {
-		List<User> users = userQueries.findAllUsers();
-		int toDelete = users.size();
-		
-		int deleted = userQueries.deleteUsers();
-		
-		assertTrue(toDelete==deleted);
-		
-		users = userQueries.findAllUsers();
-		assertTrue(users.isEmpty());
-		
-	      // Fetch database data after executing your code
-		IDatabaseConnection dc = new DatabaseConnection(dbManager.getConnection());		
-        IDataSet databaseDataSet = dc.createDataSet();
-        ITable actualTable = databaseDataSet.getTable("USERS");
-
-        // Load expected data from an XML dataset
-		InputStream is = UserQueriesTestWithDbUnit.class.getResourceAsStream("/usersEmpty.xml");
-        IDataSet expectedDataSet = new FlatXmlDataSetBuilder().build(is);
-        ITable expectedTable = expectedDataSet.getTable("USERS");
-
-        // Assert actual database table match expected table
-        new DbUnitAssert().assertEquals(expectedTable, actualTable);
-	}
-	
-	@Test
-	public void testDeleteUsersPerLogin() throws DatabaseUnitException, SQLException, FileNotFoundException {
-		
-		int deleted = userQueries.deleteUsersPerLogin("guest");
-		
-		assertTrue(deleted==1);
-		
-
-		
-	      // Fetch database data after executing your code
-		IDatabaseConnection dc = new DatabaseConnection(dbManager.getConnection());		
-        IDataSet databaseDataSet = dc.createDataSet();
-        ITable actualTable = databaseDataSet.getTable("USERS");
-
-        // Load expected data from an XML dataset
-		InputStream is = UserQueriesTestWithDbUnit.class.getResourceAsStream("/usersWithoutGuest.xml");
-        IDataSet expectedDataSet = new FlatXmlDataSetBuilder().build(is);
-        ITable expectedTable = expectedDataSet.getTable("USERS");
-
-        // Assert actual database table match expected table
-        new DbUnitAssert().assertEquals(expectedTable, actualTable);
-	}
 
 }
